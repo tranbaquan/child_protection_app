1f6538315059dc8273ed9fb271b5edd366bbe24a
dongquinguyen <cristiazlatan@gmail.com>
2019-04-04 11:32:09 +0700

    qui 4/4/2019

---
+++ lib/parent_child.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Child Protection"),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
